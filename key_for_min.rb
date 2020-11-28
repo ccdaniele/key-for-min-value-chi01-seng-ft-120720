1c5b46ba2c14262a15b74122d4f3aae4fee64706
Daniel Calderon <danieleduardocald@gmail.com>
2020-11-28 23:52:12 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,27 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
-
 def key_for_min_value(name_hash)
 
-end
+final_price = []
+  counter = 0
+  best_price = {}
+  
+  for counter in 0.. 10000 do 
+    name_hash.each { |item, price|
+     
+     if counter == price
+       best_price [item] = price
+     end
+    }
+   
+   end
+  
+  counter += 1
+  i = 0
+  for i in 0..0
+     best_price.each { |item, price|
+        final_price<< "#{item}"
+     }
+    end
+    puts final_price[0]
+  end
